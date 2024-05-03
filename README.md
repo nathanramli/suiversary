3a030fcb5c257bc684adf1b884a062d55c168181
Nathan Ramli <hezkyanatanael84@gmail.com>
2024-05-04 03:36:01 +0700

    feat: add mining tool

---
+++ README.md
@@ -0,0 +1,13 @@
+## Happy Suiversary!
+
+To celebrate Suiversary, I organize a community exclusive event to mine Suiversary NFT that's limited! Only 100 total supply!
+
+### Quick start
+
+1. Run `pnpm install` to install dependencies.
+2. Create an `.env` file with content of `.env.example`. Fill out your mnemonics.
+
+> It's recommended to run with a new wallet, and just add some SUI for gas fee 
+ (_maybe you need at least 10 SUI_).
+
+3. Run `pnpm mine` to start mining. Happy mining!
---
+++ mining/mining.ts
@@ -0,0 +1,146 @@
+import 'colorts/lib/string';
+import { SUI_CLOCK_OBJECT_ID, SuiTxBlock } from "@scallop-io/sui-kit";
+import { suiKit } from "utils/connection";
+import { publishResult } from "suiversary";
+import { SUI_TYPE_ARG, SuiObjectRef } from "@mysten/sui.js";
+
+type Coin = {
+    value: string
+} & SuiObjectRef;
+
+const refetchGas = async (gas: Coin): Promise<Coin> => {
+    const resp = await suiKit.provider().getObject({ id: gas.objectId });
+    return {
+        objectId: resp.data!.objectId,
+        version: resp.data!.version,
+        digest: resp.data!.digest,
+        value: gas.value
+    }
+}
+
+const submitProof = async (coin: Coin, gas: Coin) => {
+    try {
+        const txb = new SuiTxBlock();
+        txb.setGasPayment([{ objectId: gas.objectId, version: gas.version, digest: gas.digest  }])
+
+        const coinValue = parseInt(coin.value);
+        if (coinValue < 1e9) {
+            const newValue = txb.splitCoins(txb.gas, [1e9 - coinValue])[0];
+            txb.mergeCoins(coin.objectId, [newValue]);
+        } else if (coinValue > 1e9) {
+            const removedValue = txb.splitCoins(coin.objectId, [coinValue - 1e9])[0];
+            txb.transferObjects([removedValue], suiKit.currentAddress());
+        }
+
+        txb.moveCall(`${publishResult.packageId}::suiversary::mint_pow`,
+            [
+                txb.object(publishResult.registryId),
+                txb.object(coin.objectId),
+                txb.object(SUI_CLOCK_OBJECT_ID)
+            ],
+            []
+        );
+        return suiKit.signAndSendTxn(txb);
+    } catch (e) {
+        console.error("Error when submitting proof: ", e);
+        process.exit(0);
+    }
+}
+
+const splitObjects = async () => {
+    try {
+        const txb = new SuiTxBlock();
+        const array = new Array(500);
+        array.fill(1);
+        const arr = txb.splitCoins(txb.gas, array);
+        txb.transferObjects(arr, suiKit.currentAddress());
+        const resp = await suiKit.signAndSendTxn(txb);
+        console.log("Objects splitted: ", resp.digest)
+    } catch (e) {
+        console.error("Error when splitting objects: ", e);
+        process.exit(0);
+    }
+}        
+
+const mergeObjects = async (coins: Array<Coin>, gas: Coin) => {
+    try {
+        while (coins.length > 1) {
+            const txb = new SuiTxBlock();
+            txb.setGasPayment([{ objectId: gas.objectId, version: gas.version, digest: gas.digest }])
+
+            const subarr = coins.splice(1, 500)
+            const objectIds = subarr.map(coin => txb.object(coin.objectId))
+            txb.mergeCoins(txb.gas, objectIds)
+            const resp = await suiKit.signAndSendTxn(txb);
+
+            gas = await refetchGas(gas)
+            console.log("Objects merged: ", resp.digest)
+        }
+    } catch (e) {
+        console.error("Error when merging objects: ", e);
+        process.exit(0);
+    }
+}
+
+const observeObjects = async () => {
+    try {
+        const coins = new Array<Coin>();
+
+        let cursor = null;
+        do {
+            const resp = await suiKit.provider().getCoins({ owner: suiKit.currentAddress(), coinType: SUI_TYPE_ARG, cursor: cursor, limit: 100 });
+            resp.data.map((data) => {
+                coins.push({
+                    objectId: data.coinObjectId,
+                    version: data.version,
+                    digest: data.digest,
+                    value: data.balance
+                })
+            })
+            cursor = resp.nextCursor;
+        } while (cursor !== null);
+
+        let gas = coins[0];
+        let gasIndex = 0;
+        for (let i = 1; i < coins.length; i++) {
+            if (parseInt(coins[i].value) > parseInt(gas.value)) {
+                gas = coins[i];
+                gasIndex = i;
+            }
+        }
+
+        coins.splice(gasIndex, 1);
+
+        let counter = 0;
+        for (let i = 0; i < coins.length; i++) {
+            const coin = coins[i];
+            if (coin.objectId.startsWith("0x0000")) {
+                const resp = await submitProof(coin, gas);
+                gas = await refetchGas(gas)
+                console.log(`Proof submitted: ${resp.digest}`.green);
+                coins.splice(i--, 1);
+                counter++;
+            }
+        }
+
+        if (counter > 0) {
+            console.log(`${counter} proofs submitted! NFT minted!`.green);
+        } else {
+            console.log("No proof submitted".yellow);
+        }
+
+        await mergeObjects(coins, gas);
+    } catch (e) {
+        console.error("Error when observing objects: ", e);
+        process.exit(0);
+    }
+}
+
+const main = async () => {
+    do {
+        await observeObjects();
+        await splitObjects();    
+    } while (true);
+}
+
+main()
